eb92e798864f6f888bbdcc40aff402608219b997
juntaenoh <45313625+juntaenoh@users.noreply.github.com>
2023-01-03 10:43:34 +0900

    main/monitoring layout
    main/monitoring layout

---
+++ first_test/lib/activity_monitoring.dart
@@ -0,0 +1,238 @@
+import 'package:flutter/material.dart';
+
+class ActivityMonitoringWidget extends StatefulWidget {
+  const ActivityMonitoringWidget({Key? key}) : super(key: key);
+
+  @override
+  _ActivityMonitoringWidgetState createState() =>
+      _ActivityMonitoringWidgetState();
+}
+
+class _ActivityMonitoringWidgetState extends State<ActivityMonitoringWidget> {
+  final _unfocusNode = FocusNode();
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+  String _buttonState = "OFF";
+
+  @override
+  void dispose() {
+    _unfocusNode.dispose();
+    super.dispose();
+  }
+  void onClick() {
+    print('onClick()');
+    setState(() {
+      if (_buttonState == 'OFF') {
+        _buttonState = 'ON';
+      }
+      else {
+        _buttonState = 'OFF';
+      }
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: scaffoldKey,
+      body: SafeArea(
+        child: GestureDetector(
+          onTap: () => FocusScope.of(context).requestFocus(_unfocusNode),
+          child: Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: BoxDecoration(),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Container(
+                  width: double.infinity,
+                  height: MediaQuery.of(context).size.height * 0.07,
+                  decoration: BoxDecoration(),
+                  child: Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(0, 30, 0, 0),
+                    child: Text(
+                      '신체 밸런스 측정하기',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ),
+                Divider(
+                  thickness: 1,
+                  color: Colors.black,
+                ),
+                Container(
+                  width: double.infinity,
+                  height: MediaQuery.of(context).size.height * 0.1,
+                  decoration: BoxDecoration(),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: MediaQuery.of(context).size.width * 0.5,
+                        height: 100,
+                        decoration: BoxDecoration(),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            Padding(
+                              padding: EdgeInsetsDirectional.fromSTEB(
+                                  10, 10, 10, 10),
+                              child: Container(
+                                width: 10,
+                                height: 10,
+                                decoration: BoxDecoration(
+                                  color: Color(0xFF27FF42),
+                                  borderRadius: BorderRadius.circular(30),
+                                ),
+                              ),
+                            ),
+                            Container(
+                              width: MediaQuery.of(context).size.width * 0.4,
+                              height: 40,
+                              decoration: BoxDecoration(
+                                color: Colors.black,
+                                borderRadius: BorderRadius.circular(30),
+                              ),
+                              child: Padding(
+                                padding: EdgeInsetsDirectional.fromSTEB(
+                                    10, 10, 10, 10),
+                                child: Text(
+                                  'Scale 1',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontFamily: 'Poppins',
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  height: MediaQuery.of(context).size.height * 0.37,
+                  decoration: BoxDecoration(),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Expanded(
+                        child: Padding(
+                          padding:
+                          EdgeInsetsDirectional.fromSTEB(30, 30, 30, 30),
+                          child: Container(
+                            width: MediaQuery.of(context).size.width,
+                            height: MediaQuery.of(context).size.height * 1,
+                            decoration: BoxDecoration(
+                              color: Color(0xFFCCCCCC),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  height: MediaQuery.of(context).size.height * 0.12,
+                  decoration: BoxDecoration(),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Expanded(
+                        child: Padding(
+                          padding:
+                          EdgeInsetsDirectional.fromSTEB(30, 10, 30, 10),
+                          child: Container(
+                            width: MediaQuery.of(context).size.width,
+                            height: MediaQuery.of(context).size.height * 1,
+                            decoration: BoxDecoration(
+                              color: Color(0xFFCCCCCC),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  height: MediaQuery.of(context).size.height * 0.1,
+                  decoration: BoxDecoration(),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(10, 10, 10, 10),
+                        child: ElevatedButton(
+                            child: Text("측정하기"),
+                            onPressed: onClick,
+                            style: ElevatedButton.styleFrom(
+                                minimumSize:const Size(150, 40),
+                                backgroundColor: Color(0xFFCCCCCC),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: new BorderRadius.circular(10.0)) )
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(10, 10, 10, 10),
+                        child: ElevatedButton(
+                            child: Text(
+                              '나에게 보내기',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontFamily: 'Poppins',
+                                color: Color(0xFFCCCCCC),
+                              ),
+                            ),
+                            onPressed: onClick,
+                            style: ElevatedButton.styleFrom(
+                                minimumSize:const Size(150, 40),
+                                backgroundColor: Color(0xFFffffff),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: new BorderRadius.circular(10.0)) )
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  height: MediaQuery.of(context).size.height * 0.10,
+                  decoration: BoxDecoration(),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                          child: Text("처음으로"),
+                          onPressed: (){
+                            Navigator.pop(context);
+                          },
+                          style: ElevatedButton.styleFrom(
+                              minimumSize:const Size(130, 30),
+                              backgroundColor: Color(0xFFcccccc),
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: new BorderRadius.circular(30.0)) )
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
